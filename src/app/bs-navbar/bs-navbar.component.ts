9aa88821e207d89ede0a94416becf8f091120840
Kunal Ojha <kunalojha@Kunals-MacBook-Pro.local>
2018-02-17 18:47:52 -0500

    adding google login

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
-
-  constructor() { }
+export class BsNavbarComponent{
+  user$;
+  constructor(private afAuth: AngularFireAuth) { }
 
   ngOnInit() {
+    this.user$ = this.afAuth.authState;
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
