9011f081c295e712a1b4e7ed3ded83ceac4af208
Kunal Ojha <kunalojha@Kunals-MacBook-Pro.local>
2018-02-22 18:18:20 -0600

    shopping cart item count in navbar

--- src/app/components/bs-navbar/bs-navbar.component.ts
+++ src/app/components/bs-navbar/bs-navbar.component.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { UserService } from '../../services/user.service';
 import { AppUser } from '../../models/app-user';
+import { ShoppingCartService } from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent{
+export class BsNavbarComponent implements OnInit {
   appUser: AppUser;
+  shoppingCartItemCount: number;
 
-  constructor(private authService: AuthService) { 
-    authService.appUser$.subscribe(appUser => {this.appUser = appUser});
-  }
+  constructor(private authService: AuthService, private shoppingCartService: ShoppingCartService) { }
 
   logout() {
     this.authService.logout();
   }
 
+  async ngOnInit() {
+    this.authService.appUser$.subscribe(appUser => {this.appUser = appUser});
+    let cart$ = await this.shoppingCartService.getCart();
+    cart$.subscribe(cart => {
+      this.shoppingCartItemCount = 0;
+      for (let productId in cart.items)
+        this.shoppingCartItemCount += cart.items[productId].quantity
+      
+    })
+  }
 }
